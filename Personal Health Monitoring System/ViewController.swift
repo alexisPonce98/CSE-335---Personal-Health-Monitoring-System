d049a675632ff1198860cbb80482216e75056f20
alexisPonce <ponce.alexis98@gmail.com>
2021-04-16 19:01:04 -0700

    started the saveContext in the main viewController

--- Personal Health Monitoring System/ViewController.swift
+++ Personal Health Monitoring System/ViewController.swift
@@ -10,12 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var returnSys:String?
-    var returnDia:String?
-    var returnWeight:String?
-    var returnSug:String?
+    var returnSys:Int?
+    var returnDia:Int?
+    var returnWeight:Int?
+    var returnSug:Int?
     var returnOther:String?
     var fromAdd = false
+    let date = Date()
+    let cal = Calendar.current
+    let comp: Set<Calendar.Component> = [
+    .year,
+    .month,
+    .day,
+    .hour,
+    .minute,
+    .second
+    ]
     
     var context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     var H:healthModel?
@@ -23,8 +33,21 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let realComp = self.cal.dateComponents(self.comp, from: self.date)
+        let dateStr = "\(realComp.month!), \(realComp.day!) \(realComp.year!)"
         H = healthModel(manage: context)
-        hFetch = H?.fetch()
+        hFetch = (H?.fetch())!
+        if(fromAdd){
+            if(self.returnOther == ""){
+                H?.saveContext(day: dateStr, sys: self.returnSys!, dia: self.returnDia!, weigth: self.returnWeight!, sugar: self.returnSug!, other: "none")
+            }else{
+                H?.saveContext(day: dateStr, sys: self.returnSys!, dia: self.returnDia!, weigth: self.returnWeight!, sugar: self.returnSug!, other: self.returnOther!)
+            }
+        }
+        hFetch = (H?.fetch())!
+        for (index,val) in hFetch.enumerated(){
+            print("\(val.systolicBloodPressure)")
+        }
         
         // Do any additional setup after loading the view.
     }
