a9fa90d99fbd1ade79e83dc1b4792a2e8ee95a3c
Alexis Ponce <ponce.alexis98@gmail.com>
2021-04-16 18:11:09 -0700

    messed around with the addViewController, started the transfer of data from the addViewController and the main ViewController

--- Personal Health Monitoring System/ViewController.swift
+++ Personal Health Monitoring System/ViewController.swift
@@ -9,9 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var returnSys:String?
+    var returnDia:String?
+    var returnWeight:String?
+    var returnSug:String?
+    var returnOther:String?
+    var fromAdd = false
+    
+    var context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var H:healthModel?
+    var hFetch = [Health]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        H = healthModel(manage: context)
+        hFetch = H?.fetch()
+        
         // Do any additional setup after loading the view.
     }
 
--- Personal Health Monitoring System/addViewController.swift
+++ Personal Health Monitoring System/addViewController.swift
@@ -10,14 +10,18 @@ import UIKit
 
 class addViewController: UIViewController {
 
-    @IBOutlet weak var sys: UITextField!
     
+    @IBOutlet weak var sys: UITextField!
     @IBOutlet weak var sugar: UITextField!
     @IBOutlet weak var weight: UITextField!
     @IBOutlet weak var other: UITextField!
     @IBOutlet weak var dia: UITextField!
-    
-    
+    var getSys:String = ""
+    var sug:String = ""
+    var getWeight:String = ""
+    var getOther:String = ""
+    var getDia:String = ""
+    var noSymp = false
     override func viewDidLoad() {
         
         super.viewDidLoad()
@@ -26,17 +30,85 @@ class addViewController: UIViewController {
     }
     
     @IBAction func save(_ sender: Any) {
+        var checked = false
+        var noSymp = false
+        if(self.sys.text != ""){
+            getSys = self.sys.text!
+        }else{
+            let alert = UIAlertController(title: "Forgot to enter your systolic boold pressure", message: "please check others", preferredStyle: .alert)
+            let action = UIAlertAction(title: "OK", style: .default){(action) in
+                checked = true
+            }
+            alert.addAction(action)
+            self.present(alert, animated: true, completion: nil)
+        }
+        if(self.dia.text != ""){
+            getDia = self.dia.text!
+            self.dia.text = ""
+        }else{
+            if(!checked){
+                let alert = UIAlertController(title: "Forgot to enter your diastolic bool pressure", message: "pleas check others", preferredStyle: .alert)
+                let action = UIAlertAction(title: "OK", style: .default){(action) in
+                    checked = true
+                }
+                alert.addAction(action)
+                self.present(alert, animated: true,completion: nil)
+            }
+        }
+        if(self.weight.text != ""){
+            sug = self.weight.text!
+            self.weight.text = ""
+        }else{
+            if(!checked){
+                let alert = UIAlertController(title: "Forgot to enter your weight", message: "Please check others", preferredStyle: .alert)
+                let action = UIAlertAction(title: "OK", style: .default){(action) in
+                    checked = true
+                }
+                alert.addAction(action)
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+        if(self.sugar.text != ""){
+            getWeight = self.sugar.text!
+            self.sugar.text = ""
+        }else{
+            if(!checked){
+                let alert = UIAlertController(title: "Forgot to enter your sugar level", message: "Please check others", preferredStyle: .alert)
+                let action = UIAlertAction(title: "OK", style: .default){(action) in
+                    checked = true
+                }
+                alert.addAction(action)
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+        if(self.other.text != ""){
+            getOther = self.other.text!
+            self.other.text = ""
+        }else{
+            noSymp = true
+        }
+        checked = false
         
     }
     
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        if(segue.identifier == "enterData"){
+            let new = segue.description as? ViewController
+            new?.fromAdd = true
+            new?.returnSys = self.getSys
+            new?.returnDia = self.getDia
+            new?.returnWeight = self.getWeight
+            new?.returnOther = self.getOther
+            new?.returnSug = self.sug
+            
+        }
     }
-    */
+    
 
 }
